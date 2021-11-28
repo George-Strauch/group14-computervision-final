a3bf6310150ad35b612f1e676d68b92251643a31
Ruben <suarezjruben@users.noreply.github.com>
2021-11-27 20:37:28 -0600

    Added a delay to display all the test images and results one after the other

--- dist_calc.m
+++ dist_calc.m
@@ -15,20 +15,13 @@ function results = dist_calc(dataset, testset)
     results = zeros(size_of_test, rows2);
 
     for i = 1: size_of_test
-        %dist = inf;
-        %results(i,1) = i;
+        
         for j = 1:size_of_dataset
             test_data = testset(i,:);
             train_data = dataset(j,:);
             eu_dist = sqrt(sum(sum((test_data - train_data).^2)));
             
             results(i,j)=eu_dist;
-            
-%             if(eu_dist < dist)
-%                 dist = eu_dist;
-%                 results(i,2)=j;
-%                 results(i,3)=dist;
-%             end
 
         end
     end
--- main.m
+++ main.m
@@ -1,46 +1,40 @@
 
 % clc
 % clear all
-
+pause('on')
+set(groot,'defaultfigureposition',[400 250 900 750])
 %% Testing for Ruben
 load("G:\My Drive\School\Fall 2021\INTRO TO COMPUTER VISION\Final Project\data.mat");
-addpath("G:\My Drive\School\Fall 2021\INTRO TO COMPUTER VISION\Final Project\group14-computervision-final");
-addpath("G:\My Drive\School\Fall 2021\INTRO TO COMPUTER VISION\Final Project\group14-computervision-final\Test_Images");
+%addpath("G:\My Drive\School\Fall 2021\INTRO TO COMPUTER VISION\Final Project\group14-computervision-final");
 
 %% Testing
-top_n = 8;
+top_n = 8;  % Number of comparisons to return
+test_dogs_dir = "G:\My Drive\School\Fall 2021\INTRO TO COMPUTER VISION\Final Project\group14-computervision-final\Test_Images";
+files = dir(test_dogs_dir);
+files = files(~[files.isdir]);
+test_dog_images = files(:,1);
 
-% Chihuahua
-% img = imread('Chihuahua.jpg');
+for f=1:size(files,1)
+    img = imread(string(files(f).name));    
 
-% Dog with baby
-% img = imread('dog_with_baby.jpg');
-
-% Dog with person
-% img = imread('dog_with_person.jpg');
-
-% German shepherd
-% img = imread('German-shepherd.jpg');
-
-% Golden retriever puppy
-% img = imread('golder-retriever-puppy.jpeg');
-
-dog_features = feature_extract(img);
-similarity_array = dist_calc(features, dog_features);
-[top, indices]=mink(similarity_array, top_n);
-figure(1);
-subplot(floor(top_n/3)+1,3,1);
-imshow(img,[]);
-for i = 1:top_n
+    dog_features = feature_extract(img);
+    similarity_array = dist_calc(features, dog_features);
+    [top, indices]=mink(similarity_array, top_n);
     figure(1);
-    subplot(floor(top_n/3)+1,3,i+1);
-    imshow(imread(paths{indices(1,i)}), []);
-    %imshow(imread(paths{indices(1,i)}), []);
-    path = paths{indices(1,i)};
-    breed = '-[A-Z a-z \w]+/';
-    [start, endd] = regexp(path, breed);
-    title(strrep(path(1,start+1:endd-1),'_',' '));
+    subplot(floor(top_n/3)+1,3,1);
+    imshow(img,[]);
+    for i = 1:top_n
+        figure(1);
+        subplot(floor(top_n/3)+1,3,i+1);
+        imshow(imread(paths{indices(1,i)}), []);
+        %imshow(imread(paths{indices(1,i)}), []);
+        path = paths{indices(1,i)};
+        breed = '-[A-Z a-z \w]+/';
+        [start, endd] = regexp(path, breed);
+        title(strrep(path(1,start+1:endd-1),'_',' '));
+    end
+    
+    pause(3);
 end
-
 
 
